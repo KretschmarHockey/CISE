a03a84f481ca6fde24462798787788ad243f2ebd
Aidan Stehbens <aidanstehbens@gmail.com>
2019-05-07 14:14:56 +1200

    Added Blank Site, edited .gitignore

--- project/project/settings.py
+++ project/project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tree_store.apps.TreeStoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ project/tree_store/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TreeStoreConfig(AppConfig):
+    name = 'tree_store'
