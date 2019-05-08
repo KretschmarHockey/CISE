3267c794d94e462744a073d70fafa1470f7e0da0
Aidan Stehbens <a.stehbens@hotmail.com>
2019-05-08 16:04:47 +1200

    Added search results page

--- project/tree_store/urls.py
+++ project/tree_store/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'store'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search, name='index'),
+    path('results/', views.search_results, name='results'),
 ]
--- project/tree_store/views.py
+++ project/tree_store/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import TreeItem
 
 # Create your views here.
 def index(request):
     return render(request, 'tree_store/index.html')
+
+def search_results(request):
+    item_list = TreeItem.objects.all()
+    return render(
+        request, 
+        'tree_store/search_results.html',
+        {'results': item_list}
+    )
+
+def search(results):
+    pass
