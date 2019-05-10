3f3affae62255c8139542cbfd86fdaecbc9f8022
Josh PC <Johkey3@gmail.com>
2019-05-10 23:13:52 +1200

    Changed project name back to project

--- plantatree/store/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'store'
-urlpatterns = [
-    path('search/', views.search, name='search'),
-    path('results/', views.SearchResultsView.as_view(), name='results'),
-]
