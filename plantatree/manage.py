cbbb8d116e746f68994c8ce7d299dd0c463d7700
Josh PC <Johkey3@gmail.com>
2019-05-10 22:37:51 +1200

    Removing old project

--- plantatree/manage.py
+++ plantatree/manage.py
@@ -5,7 +5,11 @@ import sys
 
 
 def main():
+<<<<<<< HEAD
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+=======
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plantatree.settings')
+>>>>>>> 2nd Commit
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
