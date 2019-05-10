8c637ed86e8c4b2c9390a427d280d5934a3acb33
Josh PC <Johkey3@gmail.com>
2019-05-10 23:15:16 +1200

    Changed project name back to project

---
+++ project/store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'store'
+urlpatterns = [
+    path('search/', views.search, name='search'),
+    path('results/', views.SearchResultsView.as_view(), name='results'),
+]
---
+++ project/store/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import TreeItem
+from django.db.models import Q
+from functools import reduce
+
+# Create your views here.
+def search(request):
+    return render(request, 'store/search.html')
+
+class SearchResultsView(ListView):
+    template_name = 'store/search_results.html'
+    context_object_name = 'results'
+
+    def get_queryset(self):
+        return search_for_tree(self.request.GET.get('search_input', ''))
+
+def search_for_tree(keywords):
+    if keywords == '':
+        return []
+    queries = [Q(name__contains=kw) for kw in keywords.split()]
+    query = reduce(lambda curr, new: curr | new, queries)
+    return TreeItem.objects.filter(query)
