fa534c91da3092507d06c529dd6b23ce4265336f
Aidan Stehbens <aidanstehbens@gmail.com>
2019-05-19 22:15:11 +1200

    Updated store to filter items and select purchase criteria. Also added user, User: 'aidan', Pass: 'nadia' (my name backwards) and two sample data. Added another table for trees, will delete other two as they do not really fit right.

--- plantatree/store/models.py
+++ plantatree/store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+
 
 class ItemDescription(models.Model):
     name = models.CharField(max_length=50)
@@ -10,14 +12,35 @@ class ItemDescription(models.Model):
     picture_url = models.CharField(max_length=50)
 
     def get_dollars(self):
-        return price / 100
+        return self.price / 100
 
     def __str__(self):
         return '%s, $%.2f' % (self.name, self.price / 100)
+
 
 class TreeItem(models.Model):
     height = models.IntegerField(default=-1)
     age = models.IntegerField(default=-1)
     item_info = models.ForeignKey(ItemDescription,
-                on_delete=models.CASCADE)
+                                  on_delete=models.CASCADE)
 
+
+class Tree(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    url = models.CharField(max_length=50)
+    type = models.CharField(max_length=20)
+    soil = models.CharField(max_length=1)
+    sun = models.CharField(max_length=1)
+    food = models.CharField(max_length=1)
+    water = models.CharField(max_length=1)
+    pruning = models.CharField(max_length=1)
+    max_height = models.IntegerField(default=-1)
+    growth_rate = models.CharField(max_length=1)
+    price = models.IntegerField(default=-1)
+
+    def price_str(self):
+        return '$%s' % (format(self.price / 100, ',.2f'))
+
+    def __str__(self):
+        return '%s, $%.2f' % (self.name, self.price / 100)
--- plantatree/store/views.py
+++ plantatree/store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
+from store.models import Tree
 from .searching import search_for_tree
+
 
 # Create your views here.
 
@@ -12,8 +15,38 @@ def search(request):
 class SearchResultsView(ListView):
     template_name = 'store/search_results.html'
     context_object_name = 'results'
+    filter_names = [
+        'type',
+        'soil',
+        'sun',
+        'food',
+        'water',
+        'pruning',
+        'height',
+        'growth',
+        'price',
+    ]
 
     def get_queryset(self):
         keywords = self.request.GET.get('search_input', '')
-        filters = self.request.GET.get('type', '')
-        return search_for_tree(keywords, filters)
+        filters = {
+            x: self.request.GET.get(x, '')
+            for x in self.filter_names
+            if self.request.GET.get(x, '') is not '' and self.request.GET.get(x, '') != 'none'
+        }
+        return map(lambda s: ProductDisplay(s), search_for_tree(keywords, filters))
+
+
+class ProductView(DetailView):
+    model = Tree
+    context_object_name = 'context'
+    template_name = 'store/purchase.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+
+class ProductDisplay:
+    def __init__(self, data) -> None:
+        self.display = str(data)
+        self.data = data
